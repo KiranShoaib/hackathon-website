4da9dff4d543bfec66ee2c191834dfc33941df72
KiranShoaib <kiranshoaib091@gmail.com>
2024-12-23 22:04:26 +0500

    change in mobNav

--- src/components/Mob-Nav.tsx
+++ src/components/Mob-Nav.tsx
@@ -29,7 +29,7 @@ function MobNav() {
 
             <div
                 className={`fixed top-0 right-0 h-full w-40  bg-white shadow-lg transform ${isOpen ? "translate-x-0" : "translate-x-full"
-                    } transition-transform duration-300 ease-in-out`}
+                    } transition-transform duration-300 ease-in-out z-50`}
             >
 
                 <div className="flex justify-end p-4">
