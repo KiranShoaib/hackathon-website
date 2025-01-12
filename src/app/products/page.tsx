fe3171491b00080fed744b8f0d756ec9f4a2aa89
KiranShoaib <kiranshoaib091@gmail.com>
2025-01-12 14:00:12 +0500

    change in component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,12 @@
 import ProductHeader from "@/components/Product-Header";
-import ProductItems from "@/components/Product-items";
+import ProductItems from "@/components/Product-Items";
 
 function Products(){
     return (
         <div>
             <ProductHeader/>
             <ProductItems/>
+            
             
 
         </div>
